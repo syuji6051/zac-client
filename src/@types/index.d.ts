06719e50d287e92ffa5e176d68d0b420e47048ec
sokamura <sokamura@terrasky.co.jp>
2019-12-20 14:18:45 +0900

    Work Typeを追加

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,12 +1,15 @@
 import * as puppeteer from 'puppeteer';
 
-export type work = [
-
-]
+export type Work = {
+  hour: number;
+  minute: number;
+  code: string;
+  text?: string;
+}
 
 export class ZacClient {
   constructor(Page: puppeteer.Page, userId: String, password: String);
   login(): Promise<void>;
-  nippou(month: number, day: number, works: work[]): Promise<void>;
+  nippou(month: number, day: number, works: Work[]): Promise<void>;
 }
 
